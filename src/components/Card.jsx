5543f50de4ce9c493823a113c6807e3fb4bf6d1f
özcan çetin <cetin445@gmail.com>
2022-07-03 02:34:08 +0300

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import axios from 'axios'
+import {  useState, useEffect } from 'react'
+import Item from './Item'
+
+const Card = () => {
+  const [info, setInfo] = useState()
+
+    const userInfo = async () => {
+        const url = "https://randomuser.me/api/";
+
+        try {
+            const response = await axios.get(url)
+            // console.log(response);
+            const data = response.data.results[0];
+            setInfo(data);
+       
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+ useEffect(() => {
+   userInfo()
+ }, [])
+ 
+  return (
+    <div>
+        {info && <Item info={info} userInfo={userInfo}/>}
+    </div>
+  )
+}
+
+export default Card;
---
+++ src/components/Item.jsx
@@ -0,0 +1,41 @@
+import {FaEnvelopeOpenText} from "react-icons/fa";
+import {FaPhone} from "react-icons/fa";
+import {IoLocation} from "react-icons/io5";
+
+const Item = ({info, userInfo}) => {
+    const{email, name, picture, phone, location, dob, registered}=info;
+    const {title, first, last} = name;
+    const {large, medium, thumbnail} = picture;
+    const {city, country} = location;
+    const {age} = dob;
+    const {date}=registered;
+    const registerdate = date.slice(0,10)
+  return (
+    <div>
+<div className='container'>
+        <div className="header">
+            <div className="image"><img className="itemimage" src={large} alt="large" /></div>
+            <div className="fulname">{title} {first} {last}</div>
+        </div>
+    <div className="email">
+        <p className="logo">{<FaEnvelopeOpenText/>}</p>
+        <p className="item">{email}</p>
+        </div>
+    <div className="phone">
+        <p className="logo">{<FaPhone/>}</p>
+        <p className="item">{phone}</p> </div>
+    <div className="location">
+        <p className="logo">{<IoLocation/>}</p>
+        <p className="item">{city}-{country}</p></div>
+    <div className="footer">
+        <p>Age: {age}</p>
+        <p>Register Date: {registerdate}</p>
+    </div>
+    <button onClick={userInfo} className="randomUser">Random User</button>
+    </div>
+    </div>
+    
+  )
+}
+
+export default Item
